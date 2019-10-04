94fd2ff654ea9ce79228f1450256b3ead6932a61
Ana Carolina Felicio <1900281@SALAS.AULAS>
2019-10-03 21:40:14 -0300

    Alterando o código calcula parcela_9

--- com/ana/calcula_parcela.py
+++ com/ana/calcula_parcela.py
@@ -1,14 +1,14 @@
-def pagamento(valor, atraso):
-	"""Form a complex number.
-
+def valorPagamento(valor, diasAtraso):
+    """Form a complex number.
     Keyword arguments:
     real -- the real part (default 0.0)
     imag -- the imaginary part (default 0.0)
     """
-    if valor < 0:
+    if (valor < 0):
         return None
-    elif diasatraso > 0:
+    if (diasAtraso > 0):
         multa = valor * 0.03
-        adicionaltraso = valor * (diasatraso * 0.01)
-        return valor + multa + adicionalatraso
+        adicionalAtraso = valor * (diasAtraso * 0.01)
+        return valor + multa + adicionalAtraso
+    else:
         return valor
