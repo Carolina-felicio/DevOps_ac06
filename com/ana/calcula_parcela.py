d859a6380d8e320e2954b8349dc815207a1ec327
Ana Carolina Felicio <1900281@SALAS.AULAS>
2019-10-03 22:13:34 -0300

    Alterando o código calcula parcela_20

--- com/ana/calcula_parcela.py
+++ com/ana/calcula_parcela.py
@@ -5,4 +5,8 @@ def pagamento(valor, atraso):
     """ Funcao que calcula pagamento
     """
     if valor < 0:
-        return None   
+        return None
+    if atraso > 0:
+        multa = valor * 0.03
+        adicionalatraso = valor * atraso * 0.01
+        return valor + multa + adicionalatraso
