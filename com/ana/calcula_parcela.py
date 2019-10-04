a6dfc34d09168e74b4395df0549516d11a25c368
Ana Carolina Felicio <1900281@SALAS.AULAS>
2019-10-03 21:17:46 -0300

    Alterando o código calcula parcela_4

--- com/ana/calcula_parcela.py
+++ com/ana/calcula_parcela.py
@@ -2,9 +2,8 @@ def pagamento(valor, atraso):
 
     if valor < 0:
         return None
-    if diasAtraso > 0:
+    else diasatraso > 0:
         multa = valor * 0.03
         adicionalatraso = valor * (diasatraso * 0.01)
         return valor + multa + adicionalatraso
-    else:
         return valor
