5313f441e361a392077ae695cc2f340723895a85
Ana Carolina Felicio <1900281@SALAS.AULAS>
2019-10-03 21:30:34 -0300

    Alterando o código calcula parcela_8

--- com/ana/calcula_parcela.py
+++ com/ana/calcula_parcela.py
@@ -1,12 +1,10 @@
 def pagamento(valor, atraso):
-
 	"""Form a complex number.
 
     Keyword arguments:
     real -- the real part (default 0.0)
     imag -- the imaginary part (default 0.0)
     """
-
     if valor < 0:
         return None
     elif diasatraso > 0:
